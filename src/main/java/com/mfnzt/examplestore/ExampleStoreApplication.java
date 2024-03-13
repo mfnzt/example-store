2c992301f7494a2094db80350c1ae6a51fa24d64
mfnzt <10446914+mfnzt@users.noreply.github.com>
2024-03-13 17:55:29 +0100

    test(): mise en place TestContainers

---
+++ src/main/java/com/mfnzt/examplestore/ExampleStoreApplication.java
@@ -0,0 +1,13 @@
+package com.mfnzt.examplestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleStoreApplication.class, args);
+	}
+
+}
