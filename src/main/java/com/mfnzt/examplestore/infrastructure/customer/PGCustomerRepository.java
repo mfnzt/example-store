baa685215718716018de57413dd5ef2e927c5a96
mfnzt <10446914+mfnzt@users.noreply.github.com>
2024-03-15 09:59:07 +0100

    refactor(customer): simplification findById

--- src/main/java/com/mfnzt/examplestore/infrastructure/customer/PGCustomerRepository.java
+++ src/main/java/com/mfnzt/examplestore/infrastructure/customer/PGCustomerRepository.java
@@ -2,8 +2,6 @@ package com.mfnzt.examplestore.infrastructure.customer;
 
 import com.mfnzt.examplestore.domain.customer.Customer;
 import com.mfnzt.examplestore.domain.customer.CustomerSpi;
-import com.mfnzt.examplestore.infrastructure.customer.CustomerEntity;
-import com.mfnzt.examplestore.infrastructure.customer.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -29,8 +27,9 @@ public class PGCustomerRepository implements CustomerSpi {
 
     @Override
     public Customer findById(Long id) {
-        CustomerEntity customerEntity = customerRepository.findById(id).orElse(null);
-        return customerEntity != null ? customerEntity.toCustomer() : null;
+        return customerRepository.findById(id)
+                .map(CustomerEntity::toCustomer)
+                .orElse(null);
     }
 
     @Override
